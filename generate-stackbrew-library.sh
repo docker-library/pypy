2aa463572a4a8bb61e4687a6f74adc0daebd71ec
Joe Ferguson <joe@infosiftr.com>
2014-12-19 14:07:13 -0800

    Add generate-stackbrew and update scripts

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+declare -A aliases
+aliases=(
+	[3]='latest'
+)
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( */ )
+versions=( "${versions[@]%/}" )
+url='git://github.com/docker-library/pypy'
+
+echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+
+for version in "${versions[@]}"; do
+	commit="$(git log -1 --format='format:%H' -- "$version")"
+	fullVersion="$(grep -m1 'ENV PYPY_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
+	versionAliases=( $fullVersion ${fullVersion%.*} ${fullVersion%%.*} )
+	versionAliases=( "${versionAliases[@]/#/$version-}" )
+	versionAliases=( ${versionAliases[@]} $version ${aliases[$version]} )
+	
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $version"
+	done
+	
+	for variant in onbuild slim wheezy; do
+		[ -f "$version/$variant/Dockerfile" ] || continue
+		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+		echo
+		for va in "${versionAliases[@]}"; do
+			if [ "$va" = 'latest' ]; then
+				va="$variant"
+			else
+				va="$va-$variant"
+			fi
+			echo "$va: ${url}@${commit} $version/$variant"
+		done
+	done
+done
---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+	case "$version" in
+		3) pypy="pypy$version";;
+		*) pypy='pypy';;
+	esac
+	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy-2.4.0-linux64.tar.bz2">pypy-2.4.0-linux64.tar.bz2</a></td>
+	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy3-2.4.0-linux64.tar.bz2">pypy3-2.4.0-linux64.tar.bz2</a></td>
+	fullVersion="$(curl -sSL 'https://bitbucket.org/pypy/pypy/downloads' | grep -E "$pypy"'-([0-9.]+)-linux64.tar.bz2' | sed -r 's/^.*'"$pypy"'-([0-9.]+)-linux64.tar.bz2.*$/\1/' | sort -V | tail -1)"
+	
+	(
+		set -x
+		sed -ri 's/^(ENV PYPY_VERSION) .*/\1 '"$fullVersion"'/' "$version/Dockerfile"
+		#sed -ri 's/^(FROM python):.*/\1:'"$fullVersion"'/' "$version/onbuild/Dockerfile"
+	)
+done
