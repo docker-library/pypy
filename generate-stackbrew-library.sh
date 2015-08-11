eb4c3864802df9535786f4f3977badd3f9109185
Tianon Gravi <admwiggin@gmail.com>
2015-08-11 15:12:45 -0700

    Enhance "generate-stackbrew-library.sh" to only take into account changes to the Dockerfile or files from COPY in the Dockerfile for choosing the commit hash for a particular directory

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -15,7 +15,7 @@ url='git://github.com/docker-library/pypy'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' -- "$version")"
+	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 	fullVersion="$(grep -m1 'ENV PYPY_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
 	versionAliases=( $fullVersion ${fullVersion%.*} ${fullVersion%%.*} )
 	versionAliases=( "${versionAliases[@]/#/$version-}" )
@@ -28,7 +28,7 @@ for version in "${versions[@]}"; do
 	
 	for variant in onbuild slim wheezy; do
 		[ -f "$version/$variant/Dockerfile" ] || continue
-		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+		commit="$(cd "$version/$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
 		echo
 		for va in "${versionAliases[@]}"; do
 			if [ "$va" = 'latest' ]; then
