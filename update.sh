cdf5a7cd566bc418aff8eb383b431abb3ea88e50
Tianon Gravi <admwiggin@gmail.com>
2016-06-15 09:58:18 -0700

    Update to the new format of filenames for downloads (and thus update to 3.3-5.2.0-alpha1 and 2-5.3.0)

--- update.sh
+++ update.sh
@@ -13,13 +13,14 @@ pipVersion="$(curl -sSL 'https://pypi.python.org/pypi/pip/json' | awk -F '"' '$2
 
 for version in "${versions[@]}"; do
 	case "$version" in
-		3) pypy="pypy$version";;
-		*) pypy='pypy';;
+		3) pypy="pypy3.3" ;;
+		2) pypy='pypy2' ;;
+		*) echo >&2 "error: unknown pypy variant $version"; exit 1 ;;
 	esac
 	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy-2.4.0-linux64.tar.bz2">pypy-2.4.0-linux64.tar.bz2</a></td>
 	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy3-2.4.0-linux64.tar.bz2">pypy3-2.4.0-linux64.tar.bz2</a></td>
-	fullVersion="$(curl -sSL 'https://bitbucket.org/pypy/pypy/downloads' | grep -E "$pypy"'-([0-9.]+)-linux64.tar.bz2' | sed -r 's/^.*'"$pypy"'-([0-9.]+)-linux64.tar.bz2.*$/\1/' | sort -V | tail -1)"
-	
+	fullVersion="$(curl -sSL 'https://bitbucket.org/pypy/pypy/downloads' | grep -E "$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2' | sed -r 's/^.*'"$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2.*$/\1/' | sort -V | tail -1)"
+
 	(
 		set -x
 		sed -ri '
