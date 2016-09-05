9a3996374a56828c454c6e076dda5c5fb66957fc
Jamie Hewland <jhewland@gmail.com>
2016-09-05 09:38:35 +0200

    Add command to fetch SHA256 checksum to update.sh

--- update.sh
+++ update.sh
@@ -21,10 +21,19 @@ for version in "${versions[@]}"; do
 	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy3-2.4.0-linux64.tar.bz2">pypy3-2.4.0-linux64.tar.bz2</a></td>
 	fullVersion="$(curl -sSL 'https://bitbucket.org/pypy/pypy/downloads' | grep -E "$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2' | sed -r 's/^.*'"$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2.*$/\1/' | sort -V | tail -1)"
 
+	# <p>pypy2.7-5.4.0 sha256:</p>
+	# <pre class="literal-block">
+	# ...
+	# bdfea513d59dcd580970cb6f79f3a250d00191fd46b68133d5327e924ca845f8  pypy2-v5.4.0-linux64.tar.bz2
+	# ...
+	# </pre>
+	sha256sum="$(curl -sSL 'http://pypy.org/download.html' | grep -m1 -E '[a-f0-9]{64}  '"$pypy-v$fullVersion"'-linux64.tar.bz2' | cut -d' ' -f1)"
+
 	(
 		set -x
 		sed -ri '
 			s/^(ENV PYPY_VERSION) .*/\1 '"$fullVersion"'/;
+			s/^(ENV PYPY_SHA256SUM) .*/\1 '"$sha256sum"'/;
 			s/^(ENV PYTHON_PIP_VERSION) .*/\1 '"$pipVersion"'/;
 		' "$version"{,/slim}/Dockerfile
 		sed -ri 's/^(FROM pypy):.*/\1:'"$version"'/' "$version/onbuild/Dockerfile"
