52cb11e875e2054b6de9293a6f4577256e5dc8ff
Tianon Gravi <admwiggin@gmail.com>
2017-06-08 09:37:44 -0700

    Modify "update.sh" to only consider releases whose SHA256 values are available

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -Eeuo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
@@ -9,7 +9,7 @@ if [ ${#versions[@]} -eq 0 ]; then
 fi
 versions=( "${versions[@]%/}" )
 
-pipVersion="$(curl -sSL 'https://pypi.python.org/pypi/pip/json' | awk -F '"' '$2 == "version" { print $4 }')"
+pipVersion="$(curl -fsSL 'https://pypi.org/pypi/pip/json' | jq -r .info.version)"
 
 for version in "${versions[@]}"; do
 	case "$version" in
@@ -17,17 +17,43 @@ for version in "${versions[@]}"; do
 		2) pypy='pypy2' ;;
 		*) echo >&2 "error: unknown pypy variant $version"; exit 1 ;;
 	esac
+
 	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy-2.4.0-linux64.tar.bz2">pypy-2.4.0-linux64.tar.bz2</a></td>
 	# <td class="name"><a class="execute" href="/pypy/pypy/downloads/pypy3-2.4.0-linux64.tar.bz2">pypy3-2.4.0-linux64.tar.bz2</a></td>
-	fullVersion="$(curl -sSL 'https://bitbucket.org/pypy/pypy/downloads/' | grep -E "$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2' | sed -r 's/^.*'"$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2.*$/\1/' | sort -V | tail -1)"
+	IFS=$'\n'
+	tryVersions=( $(
+		curl -fsSL 'https://bitbucket.org/pypy/pypy/downloads/' \
+			| grep -E "$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2' \
+			| sed -r 's/^.*'"$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2.*$/\1/' \
+			| sort -rV
+	) )
+	unset IFS
 
-	# <p>pypy2.7-5.4.0 sha256:</p>
-	# <pre class="literal-block">
-	# ...
-	# bdfea513d59dcd580970cb6f79f3a250d00191fd46b68133d5327e924ca845f8  pypy2-v5.4.0-linux64.tar.bz2
-	# ...
-	# </pre>
-	sha256sum="$(curl -sSL 'http://pypy.org/download.html' | grep -m1 -E '[a-f0-9]{64}  '"$pypy-v$fullVersion"'-linux64.tar.bz2' | cut -d' ' -f1)"
+	fullVersion=
+	sha256sum=
+	for tryVersion in "${tryVersions[@]}"; do
+		# <p>pypy2.7-5.4.0 sha256:</p>
+		# <pre class="literal-block">
+		# ...
+		# bdfea513d59dcd580970cb6f79f3a250d00191fd46b68133d5327e924ca845f8  pypy2-v5.4.0-linux64.tar.bz2
+		# ...
+		# </pre>
+		if \
+			sha256sum="$(
+				curl -fsSL 'http://pypy.org/download.html' \
+					| grep -m1 -E '[a-f0-9]{64}  '"$pypy-v$tryVersion"'-linux64.tar.bz2' \
+					| cut -d' ' -f1
+			)" \
+			&& [ -n "$sha256sum" ] \
+		; then
+			fullVersion="$tryVersion"
+			break
+		fi
+	done
+	if [ -z "$fullVersion" ]; then
+		echo >&2 "error: cannot find suitable release for '$version'"
+		exit 1
+	fi
 
 	(
 		set -x
