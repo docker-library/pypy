ae4d6e7d038eb080a55fc3d8d91594505e0f4030
Tianon Gravi <admwiggin@gmail.com>
2017-08-24 16:16:04 -0700

    Add support for multiple architectures

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@ set -Eeuo pipefail
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+source '.architectures-lib'
+
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
 	versions=( */ )
@@ -11,10 +13,32 @@ versions=( "${versions[@]%/}" )
 
 pipVersion="$(curl -fsSL 'https://pypi.org/pypi/pip/json' | jq -r .info.version)"
 
+sha256s="$(curl -fsSL 'http://pypy.org/download.html')"
+scrapeSha256() {
+	local pypy="$1"; shift
+	local fullVersion="$1"; shift
+	local arch="$1"; shift
+
+	# <p>pypy2.7-5.4.0 sha256:</p>
+	# <pre class="literal-block">
+	# ...
+	# bdfea513d59dcd580970cb6f79f3a250d00191fd46b68133d5327e924ca845f8  pypy2-v5.4.0-linux64.tar.bz2
+	# ...
+	# </pre>
+	echo "$sha256s" \
+		| grep -m1 -E '[a-f0-9]{64}  '"$pypy-v$tryVersion"'-'"$arch"'.tar.bz2' \
+		| cut -d' ' -f1
+}
+
+# see http://stackoverflow.com/a/2705678/433558
+sed_escape_rhs() {
+	echo "$@" | sed -e 's/[\/&]/\\&/g' | sed -e ':a;N;$!ba;s/\n/\\n/g'
+}
+
 for version in "${versions[@]}"; do
 	case "$version" in
-		3) pypy='pypy3' ;;
-		2) pypy='pypy2' ;;
+		3) pypy='pypy3'; cmd='pypy3' ;;
+		2) pypy='pypy2'; cmd='pypy' ;;
 		*) echo >&2 "error: unknown pypy variant $version"; exit 1 ;;
 	esac
 
@@ -23,8 +47,7 @@ for version in "${versions[@]}"; do
 	IFS=$'\n'
 	tryVersions=( $(
 		curl -fsSL 'https://bitbucket.org/pypy/pypy/downloads/' \
-			| grep -E "$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2' \
-			| sed -r 's/^.*'"$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2.*$/\1/' \
+			| sed -rn 's/^.*'"$pypy"'-v([0-9.]+(-alpha[0-9]*)?)-linux64.tar.bz2.*$/\1/gp' \
 			| sort -rV
 	) )
 	unset IFS
@@ -32,18 +55,8 @@ for version in "${versions[@]}"; do
 	fullVersion=
 	sha256sum=
 	for tryVersion in "${tryVersions[@]}"; do
-		# <p>pypy2.7-5.4.0 sha256:</p>
-		# <pre class="literal-block">
-		# ...
-		# bdfea513d59dcd580970cb6f79f3a250d00191fd46b68133d5327e924ca845f8  pypy2-v5.4.0-linux64.tar.bz2
-		# ...
-		# </pre>
 		if \
-			sha256sum="$(
-				curl -fsSL 'http://pypy.org/download.html' \
-					| grep -m1 -E '[a-f0-9]{64}  '"$pypy-v$tryVersion"'-linux64.tar.bz2' \
-					| cut -d' ' -f1
-			)" \
+			sha256sum="$(scrapeSha256 "$pypy" "$tryVersion" 'linux64')" \
 			&& [ -n "$sha256sum" ] \
 		; then
 			fullVersion="$tryVersion"
@@ -55,13 +68,32 @@ for version in "${versions[@]}"; do
 		exit 1
 	fi
 
-	(
-		set -x
-		sed -ri '
-			s/^(ENV PYPY_VERSION) .*/\1 '"$fullVersion"'/;
-			s/^(ENV PYPY_SHA256SUM) .*/\1 '"$sha256sum"'/;
-			s/^(ENV PYTHON_PIP_VERSION) .*/\1 '"$pipVersion"'/;
-		' "$version"{,/slim}/Dockerfile
-		sed -ri 's/^(FROM pypy):.*/\1:'"$version"'/' "$version/onbuild/Dockerfile"
-	)
+	echo "$version: $fullVersion"
+
+	linuxArchCase='dpkgArch="$(dpkg --print-architecture)"; '$'\\\n'
+	linuxArchCase+=$'\t''case "${dpkgArch##*-}" in '$'\\\n'
+	for dpkgArch in $(dpkgArches); do
+		pypyArch="$(dpkgToPyPyArch "$dpkgArch")"
+		sha256="$(scrapeSha256 "$pypy" "$fullVersion" "$pypyArch")" || :
+		if [ -z "$sha256" ]; then
+			echo >&2 "warning: cannot find sha256 for $pypy-$fullVersion on arch $pypyArch ($dpkgArch); skipping it"
+			continue
+		fi
+		bashbrewArch="$(dpkgToBashbrewArch "$dpkgArch")"
+		linuxArchCase+="# $bashbrewArch"$'\n'
+		linuxArchCase+=$'\t\t'"$dpkgArch) pypyArch='$pypyArch'; sha256='$sha256' ;; "$'\\\n'
+	done
+	linuxArchCase+=$'\t\t''*) echo >&2 "error: current architecture ($dpkgArch) does not have a corresponding PyPy $PYPY_VERSION binary release"; exit 1 ;; '$'\\\n'
+	linuxArchCase+=$'\t''esac'
+
+	for variant in '' slim; do
+		sed -r \
+			-e 's!%%PYPY_VERSION%%!'"$fullVersion"'!g' \
+			-e 's!%%TAR%%!'"$pypy"'!g' \
+			-e 's!%%CMD%%!'"$cmd"'!g' \
+			-e 's!%%ARCH-CASE%%!'"$(sed_escape_rhs "$linuxArchCase")"'!g' \
+			"Dockerfile${variant:+-$variant}.template" > "$version/$variant/Dockerfile"
+	done
+
+	sed -ri 's/^(FROM pypy):.*/\1:'"$version"'/' "$version/onbuild/Dockerfile"
 done
